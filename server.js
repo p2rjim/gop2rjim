6979e4eaf1caddff4af641666d72d5d795a11cad
p2rjim <rla010318@gmail.com>
2026-05-13 10:16:07 +0900

    Force Render host binding

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
-process.env.HOST = process.env.HOST || '0.0.0.0'
-process.env.HOSTNAME = process.env.HOSTNAME || process.env.HOST || '0.0.0.0'
+process.env.HOST = '0.0.0.0'
+process.env.HOSTNAME = '0.0.0.0'
 process.env.PORT = process.env.PORT || '3000'
 process.env.APP_DATA_DIR = process.env.APP_DATA_DIR || process.env.RENDER_DISK_MOUNT_PATH || '/tmp/gop2rjim'
 
