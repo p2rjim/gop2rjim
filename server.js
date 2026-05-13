b95cc5246a1e9ce09903a56b52a8d420a3670083
p2rjim <rla010318@gmail.com>
2026-05-13 09:49:08 +0900

    첫번째 배포 준비 완료

---
+++ server.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+process.env.HOST = process.env.HOST || '0.0.0.0'
+process.env.HOSTNAME = process.env.HOSTNAME || process.env.HOST || '0.0.0.0'
+process.env.PORT = process.env.PORT || '3000'
+process.env.APP_DATA_DIR = process.env.APP_DATA_DIR || process.env.RENDER_DISK_MOUNT_PATH || '/tmp/gop2rjim'
+
+require(path.join(__dirname, 'frontend', 'dist', 'standalone', 'server.js'))
